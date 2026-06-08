f27394db88608c5e4f4c507261cbfaf1198f4475
Adhithya J <162481176+Adhhhithya@users.noreply.github.com>
2026-06-08 11:16:25 +0530

    Time: 1 ms (38.62%), Space: 44.9 MB (39.6%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -2,17 +2,11 @@ class Solution {
     public int minCostClimbingStairs(int[] cost) {
         int n = cost.length;
         int dp[] = new int[n+1];
-        Arrays.fill(dp,-1);
-        return Math.min(func(n-1,cost,dp),func(n-2,cost,dp));
-    }
-    private static int func(int ind,int a[],int dp[]){
-        if(ind==0) return a[0];
-        if(ind==1) return a[1];
-        if(dp[ind]!=-1){
-            return dp[ind];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for(int i=2;i<n;i++){
+            dp[i] = cost[i] + Math.min(dp[i-1],dp[i-2]);
         }
-        int one =  func(ind-1,a,dp);
-        int two = func(ind-2,a,dp);
-        return dp[ind]=a[ind]+ Math.min(one,two);
+        return Math.min(dp[n-1],dp[n-2]);
     }
 }
