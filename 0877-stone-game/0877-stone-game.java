5c4774f0b7bb606a30fd8e78fa3f226e53c5e57a
Adhithya J <162481176+Adhhhithya@users.noreply.github.com>
2026-06-08 21:18:47 +0530

    Time: 7 ms (22.77%), Space: 47 MB (9.68%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -2,19 +2,30 @@ class Solution {
     public boolean stoneGame(int[] piles) {
         int n = piles.length;
         int dp[][] = new int[n][n];
-        for(int row[]:dp){
-            Arrays.fill(row,-1);
+        for(int i=n-1;i>=0;i--){
+            for(int j=i;j<n;j++){
+                if(i==j){
+                    dp[i][j] =piles[i];
+                    continue;
+                }
+                int left = piles[i] - dp[i+1][j];
+                int right = piles[j] - dp[i][j-1];
+                dp[i][j] = Math.max(left,right);
+            }
         }
-        int net_margin = func(0,n-1,piles,dp,n);
-        return (net_margin >0);
-    }
-    private static int func(int i,int j,int arr[],int dp[][],int n){
-        if(i>j) return 0;
-        if(dp[i][j]!=-1){
-            return dp[i][j];
+        if(dp[0][n-1]>0){
+            return true;
+        }else{
+            return false;
         }
-        int left = arr[i] - func(i+1,j,arr,dp,n);
-        int right = arr[j] - func(i,j-1,arr,dp,n);
-        return dp[i][j] = Math.max(left,right);
     }
+    // private static int func(int i,int j,int arr[],int dp[][],int n){
+    //     if(i>j) return 0;
+    //     if(dp[i][j]!=-1){
+    //         return dp[i][j];
+    //     }
+    //     int left = arr[i] - func(i+1,j,arr,dp,n);
+    //     int right = arr[j] - func(i,j-1,arr,dp,n);
+    //     return dp[i][j] = Math.max(left,right);
+    // }
 }
