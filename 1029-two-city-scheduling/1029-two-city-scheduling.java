a1cf3e96e7ac4372f819bd1a525fd8004e4bc64d
Adhithya J <162481176+Adhhhithya@users.noreply.github.com>
2026-06-08 23:01:21 +0530

    Time: 2 ms (96.35%), Space: 43.7 MB (16.81%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.java
+++ 1029-two-city-scheduling/1029-two-city-scheduling.java
@@ -2,9 +2,10 @@ class Solution {
     public int twoCitySchedCost(int[][] costs) {
         int n = costs.length;
         Arrays.sort(costs,(a,b)->
-            (a[0]-a[1]) - (b[0]-b[1])
+        (a[0]-a[1]) - (b[0]-b[1])
         );
         int total_cost =0;
+
         for(int i=0;i<n/2;i++){
             total_cost += costs[i][0];
         }
