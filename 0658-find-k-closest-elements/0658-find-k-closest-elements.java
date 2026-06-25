c130ef43a49cd63df3c40ac2b6f0cc681347cbd9
Adhithya J <162481176+Adhhhithya@users.noreply.github.com>
2026-06-25 15:12:23 +0530

    Time: 13 ms (39.38%), Space: 47.4 MB (94.95%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -1,35 +1,39 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        int i = findFloorInd(arr,x);
+        int i = find_target_ind(arr,x);
         int j = i+1;
-        List<Integer> temp = new ArrayList<>();
+        List<Integer> ans = new ArrayList<>();
+
         while(k>0 && i>=0 && j<arr.length){
-            if(Math.abs(x - arr[i])<= Math.abs(x - arr[j])){
-                temp.add(arr[i]);
+            if(Math.abs(x-arr[i])<=Math.abs(x-arr[j])){
+                ans.add(arr[i]);
                 i--;
             }else{
-                temp.add(arr[j]);
+                ans.add(arr[j]);
                 j++;
             }
             k--;
         }
+
         while(k>0 && i>=0){
-            temp.add(arr[i]);
+            ans.add(arr[i]);
             i--;
             k--;
         }
+
         while(k>0 && j<arr.length){
-            temp.add(arr[j]);
+            ans.add(arr[j]);
             j++;
             k--;
         }
-        Collections.sort(temp);
-        return temp;
+        Collections.sort(ans);
+        return ans;
     }
 
-    private static int findFloorInd(int arr[],int x){
-        int start = 0;
-        int end = arr.length -1;
+    private static int find_target_ind(int arr[],int x){
+        int n = arr.length;
+        int start =0;
+        int end = n-1;
         int ans =0;
         while(start<=end){
             int mid = (start+end)/2;
@@ -37,7 +41,7 @@ class Solution {
                 return mid;
             }else if(arr[mid]<x){
                 ans = mid;
-                start = mid + 1;
+                start = mid+1;
             }else{
                 end = mid -1;
             }
